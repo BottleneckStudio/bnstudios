0cfb89391a606231aa379adf72b98c1c1bd736cb
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2026-02-03 11:21:03 +0800

    test: added more failing test cases for creating logs

--- packages/logger/__tests__/logger.test.js
+++ packages/logger/__tests__/logger.test.js
@@ -16,4 +16,28 @@ describe('Logger Creation', () => {
     expect(typeof logger.flush).toBe('function');
     expect(typeof logger.isLevelEnabled).toBe('function');
   });
+
+  it('should return a frozen object', () => {
+    const logger = createLogger();
+
+    expect(Object.isFrozen(logger)).toBe(true);
+  });
+
+  it('should create a logger with custom options', () => {
+  });
+
+  it('should create a child logger with bindings', () => {
+  });
+
+  it('should throw when creating child logger with invalid bindings', () => {
+  });
+
+  it('should call flush without errors', () => {
+  });
+
+  it('should report level enabled status correctly', () => {
+  });
+
+  it('should not throw when calling logging methods', () => {
+  });
 });
