5fa7c1622f35cc9fa6b18fdaad3e70d180811897
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2026-02-03 11:43:23 +0800

    chore: fixed failing tests for creating logger

--- packages/logger/lib/logger.js
+++ packages/logger/lib/logger.js
@@ -2,6 +2,7 @@
 
 const pino = require('pino');
 const { buildLoggerConfig } = require('./config');
+const { createStream } = require('./streams');
 
 function createLoggerInstance(config) {
   const pinoOptions = {
@@ -9,6 +10,34 @@ function createLoggerInstance(config) {
     timestamp: pino.stdTimeFunctions.isoTime,
     base: { service: config.serviceName }
   };
+
+  const stream = createStream({
+    logFile: config.logFile,
+    prettyPrint: config.prettyPrint
+  });
+
+  const pinoLogger = pino(pinoOptions, stream);
+
+  return Object.freeze({
+    trace: (...args) => pinoLogger.trace(...args),
+    debug: (...args) => pinoLogger.debug(...args),
+    info: (...args) => pinoLogger.info(...args),
+    warn: (...args) => pinoLogger.warn(...args),
+    error: (...args) => pinoLogger.error(...args),
+    fatal: (...args) => pinoLogger.fatal(...args),
+    child: (bindings) => {
+      if (!bindings || typeof bindings !== 'object') {
+        throw new Error('Bindings must be a non-null object');
+      }
+      return pinoLogger.child(bindings);
+    },
+    flush: () => {
+      if (typeof pinoLogger.flush === 'function') {
+        pinoLogger.flush();
+      }
+    },
+    isLevelEnabled: (level) => pinoLogger.isLevelEnabled(level)
+  });
 }
 
 module.exports = Object.freeze({
