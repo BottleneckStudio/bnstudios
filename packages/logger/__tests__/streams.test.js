94d76e40b4fa7795116831541c47d37d68636ca4
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2026-02-03 10:42:34 +0800

    test: added failing test case for supporting streams when logging

---
+++ packages/logger/__tests__/streams.test.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const fs = require('node:fs');
+const path = require('node:path');
+const pino = require('pino');
+
+jest.mock('pino', () => {
+  const dest = jest.fn(() => ({ write: jest.fn() }));
+  return { destination: dest };
+});
+
+jest.mock('pino-pretty', () => {
+  return jest.fn(() => ({ write: jest.fn() }));
+});
+
+describe('Create Logfile or Streams', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return stdout destination by default', () => {
+    createStream(); ????
+
+      expect(pino.destination).toHaveBeenCalledWith(1);
+  });
+
+  it('should return stdout destination when logFile is not enabled', () => {
+
+  });
+
+  it('should return pretty print stream when prettyPrint is true', () => {
+
+  });
+
+  it('should return file destination when logFile is enabled', () => {
+
+  });
+
+  it('should create directory if it does not exist', () => {
+
+  });
+
+  it('should prefer file stream over pretty print', () => {
+
+  });
+});
